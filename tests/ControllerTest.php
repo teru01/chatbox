b46ccd24af5202a38dcaaf713ffbe679595c0838
teruya <abas.trace.on@gmail.com>
2018-04-04 18:31:24 +0900

    ViewとControllerのTestを追加

---
+++ tests/ControllerTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ControllerTest extends TestCase{
+
+    public function setUp(){
+
+    }
+
+}
--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -3,6 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 class ViewTest extends TestCase{
     private $view;
+
     public function setUp(){
         $this->view = new View("views", null);
 
