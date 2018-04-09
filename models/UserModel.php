d167ca7ac2a1556cf4ef04ee8ae6d0736ad2ae6b
teruya <abas.trace.on@gmail.com>
2018-04-09 20:31:17 +0900

    userModelを追加

---
+++ models/UserModel.php
@@ -0,0 +1,46 @@
+<?php
+class UserModel extends ExecuteModel{
+
+    /**
+     * ユーザをDBに登録する
+     * @param string $user_name
+     * @param string $password
+     */
+    public function insert(string $user_name, string $password){
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $now = new DateTime();
+        $sql = "INSERT INTO user(user_name, password, time_stamp) VALUES(:user_name, :password, :time_stamp)";
+        $stmt = $this->execute($sql, [
+                ':user_name'  => $user_name,
+                ':password'   => $password,
+                ':time_stamp' => $now->format('Y-m-d H:i:s'),
+            ]);
+    }
+
+    /**
+     * DBからユーザの情報を取得する
+     * @param string $user_name
+     * @return mixed
+     */
+    public function getUserRecord(string $user_name){
+        $sql = "SELECT * FROM user WHERE user_name = :user_name";
+        $userData = $this->getRecord($sql, [':user_name' => $user_name]);
+        return $userData;
+    }
+
+    /**
+     * user_nameが重複していないか否か
+     * @param string $user_name
+     * @return bool
+     */
+    public function isNotOverlapUserName(string $user_name){
+        $sql = "SELECT COUNT(id) AS count FROM user WHERE user_name = :user_name";
+        $row = $this->getRecord($sql, [':user_name' => $user_name]);
+        if($row['count'] === '0'){
+            return true;
+        }
+        return false;
+    }
+
+
+}
