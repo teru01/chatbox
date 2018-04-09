7b754f16cc953c77ab3a331db0e68afd32fb95f0
teruya <abas.trace.on@gmail.com>
2018-04-09 19:39:00 +0900

    BlogAppを作成

---
+++ BlogApp.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'MysqlAccessData.php';
+
+class BlogApp extends AppBase{
+
+    /**
+     * DBに接続する
+     */
+    protected function doDbConnection(){
+        $this->_connectModel->connect('master', [
+            'string'   =>  'mysql:dbname=weblog;host=localhost; charset=utf8',
+            'user'     =>  MYSQL_USER,
+            'password' =>  MYSQL_PASS,
+        ]);
+    }
+
+    public function getRootDirectory(){
+        return dirname(__FILE__);
+    }
+
+    protected function getRouteDefinition(){
+        return [
+            '/account'
+                => ['controller' => 'account',
+                    'action'     => 'index'],
+            '/account/:action'
+                => ['controller' => 'account'],
+            '/follow'
+                => ['controller' => 'account',
+                    'action'     => 'follow'],
+            '/'
+                => ['controller' => 'blog',
+                    'action'     => 'index'],
+            '/status/post'
+                => ['controller' => 'blog',
+                    'action'     => 'post'],
+            'user/:user_name'
+                => ['controller' => 'blog',
+                    'action'     => 'user'],
+            'user/:user_name/status/:id'
+                => ['controller' => 'blog',
+                    'action'     => 'specific'],
+        ];
+    }
+
+
+}
