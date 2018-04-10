a142e667b543dd93e1909005bb66c3c174930c38
teruya <abas.trace.on@gmail.com>
2018-04-10 21:56:14 +0900

    postActionの作成

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -16,7 +16,7 @@ class BlogController extends Controller {
     public function indexAction() {
         $user = $this->_session->get(self::USER);
         $user_data  = $this->_connect_model
-                     ->get('Status')
+                     ->get(self::STATUSMODEL_PREF)
                      ->getUserData($user[self::ID]);
 
         $index_view = $this->render([
@@ -25,5 +25,39 @@ class BlogController extends Controller {
             self::TOKEN    => $this->getToken(self::POST)
         ]);
         return $index_view;
+    }
+
+    public function postAction(){
+        if(!$this->_request->isPost()){
+            $this->httpNotFound();
+        }
+
+        $token = $this->_request->getPost(self::TOKEN);
+        if(!$this->checkToken(self::POST, $token)){
+            return $this->redirect('/');
+        }
+        $message = $this->_request->getPost(self::MESSAGE);
+        $errors = [];
+
+        if(!strlen($message)){
+            $errors[] = '投稿記事を入力してください。';
+        }elseif (mb_strlen($message) > 200){
+            $errors[] = '内容は200文字以内です。';
+        }
+
+        if(count($errors) === 0){
+            $user = $this->_session->get(self::USER);
+            $this->_connect_model->get(self::STATUSMODEL_PREF)->insert($user[self::ID], $message);
+            return $this->redirect('/');
+        }
+
+        $user = $this->_session->get(self::USER);
+        $post_data = $this->_connect_model->get(self::STATUSMODEL_PREF)->getUserData($user[self::ID]);
+        return $this->render([
+            'errors' => $errors,
+            self::MESSAGE => $message,
+            self::STATUSES => $post_data,
+            self::TOKEN => $this->getToken(self::POST)
+        ], 'index');
     }
 }
