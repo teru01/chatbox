2d725452d24dd02916bb1379505885e92fd889c6
teruya <abas.trace.on@gmail.com>
2018-04-10 20:46:57 +0900

    投稿ページのビューを作成

---
+++ views/blog/status.php
@@ -0,0 +1,13 @@
+<div class="status">
+    <div class="status_content">
+        <a href="<?php print $base_url; ?>/user/<?php print $this->escape($status['user_name']); ?>">
+            <?php print $this->escape($status['user_name']); ?>
+        </a>
+        <?php print $this->escape($status['message']); ?>
+    </div>
+    <div>
+        <a href="<?php print $base_url; ?>/user/<?php print $this->escape($status['user_name']); ?>/status/<?php print $this->escape($status['id']); ?>>">
+            <?php print $this->escape($status['time_stamp']); ?>
+        </a>
+    </div>
+</div>
