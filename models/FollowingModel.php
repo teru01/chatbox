6bfcfc26934f5b1d7fa2b007725addb2851c8bb0
teruya <abas.trace.on@gmail.com>
2018-04-11 19:41:25 +0900

    フォローに関するモデルクラスを作成

---
+++ models/FollowingModel.php
@@ -0,0 +1,30 @@
+<?php
+class FollowingModel extends ExecuteModel{
+
+    /**
+     * フォローする人をされる人のIDをDBに登録
+     * @param int $user_id
+     * @param int $follow_id
+     */
+    public function registerFollowUser(int $user_id, int $follow_id){
+        $sql = "INSERT INTO followingUser VALUES (:user_id, :follow_id)";
+        $this->execute($sql, [':user_id' => $user_id, ':follow_id' => $follow_id]);
+    }
+
+    /**
+     * 指定されたユーザーをフォローしているか
+     * @param int $user_id
+     * @param int $follow_id
+     * @return bool
+     */
+    public function isFollowUser(int $user_id, int $follow_id){
+        $sql = "SELECT COUNT(user_id) AS count
+                FROM followingUser 
+                WHERE user_id = :user_id AND following_id = :follow_id";
+        $dat = $this->getRecord($sql, [':user_id' => $user_id, ':follow_id' => $follow_id]);
+        if($dat['count'] !== '0'){
+            return true;
+        }
+        return false;
+    }
+}
