1e51b50280254ae43f65419971d3dcb59ae3bfd4
teruya <abas.trace.on@gmail.com>
2018-07-19 22:27:53 +0900

    jqueryを使わない記法に変更

--- mvc_docroot/js/showArticleMenu.js
+++ mvc_docroot/js/showArticleMenu.js
@@ -1,7 +1,18 @@
-$(function () {
-    $('.arrow').click(function (){
-        let n = $('.arrow').index(this);
-        $('.article_menu_wrapper').eq(n).slideToggle('fast');
-        return false;
-    });
-});
+const switch_article_menu = (i) => {
+    const menu = document.getElementsByClassName('article_menu_wrapper')[i];
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none';
+    }else {
+        menu.style.display = 'block';
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const it = Array.from(document.getElementsByClassName('arrow')).entries();
+    for(let elm of it){
+        elm[1].addEventListener('click', () => {
+            switch_article_menu(elm[0]);
+        }, false);
+    }
+}, false);
